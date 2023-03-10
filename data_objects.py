8f8ca67aa0d6a616211b7f5a549b7d1254480b0a
daria.gorneva <dariagorneva@gmail.com>
2023-03-10 13:35:39 +0100

    improved basic functionality

--- data_objects.py
+++ data_objects.py
@@ -3,14 +3,23 @@ import enum
 import typing as t
 
 
-class Category(enum.Enum):
+class Category(enum.Enum):  # заранее известны категории покупок
     cafe = 1
-    food = 2
+    subscriptions = 2
+    household = 3
+    beauty = 4
+    health = 5
+    entertainment = 6
+    documentation = 7
+    holidays = 8
+    transport = 9
+    rent = 10
+    food = 11
 
 
 @dataclasses.dataclass
-class Purchase:
+class Purchase:     # здесь храним информацию о пользователе и покупке
     is_closed: bool
     category: t.Optional[Category] = None
-    price: t.Optional[int] = None
+    price: t.Optional[str] = None
     name: t.Optional[str] = None
