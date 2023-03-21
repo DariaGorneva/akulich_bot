3d3f3c94e3190a5b60ed8784111e4321536a1401
Daria <dariagorneva@gmail.com>
2023-03-21 12:20:09 +0100

    add server on FastApi

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import threading
 from dotenv import load_dotenv
 from src.database import Database
 load_dotenv()
@@ -6,16 +7,27 @@ from src.config import Configuration
 import logging
 import time
 from telebot import telebot, TeleBot
+from fastapi import FastAPI
+import uvicorn
 
 
-if __name__ == '__main__':
+app = FastAPI(
+    title='Bot'
+)     # экземпляр приложения
+
+
+def start_server():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+def start_bot():
     bot: TeleBot = TeleBot(Configuration.TG_TOKEN, skip_pending=True)
     db = Database()
     init(bot, db)
-
     while True:
         try:
             logging.info("Bot running..")
+
             print('Running')
             bot.polling(none_stop=True, interval=2)
             break
@@ -26,3 +38,23 @@ if __name__ == '__main__':
             time.sleep(15)
 
             logging.info("Running again!")
+    return {'ok'}
+
+
+@app.get("/")
+async def response():
+    return {'message': 'Bot running'}
+
+
+if __name__ == '__main__':
+    # Создаем два потока - для сервера и для телеграм-бота
+    server_thread = threading.Thread(target=start_server)
+    bot_thread = threading.Thread(target=start_bot)
+
+    # Запускаем потоки
+    server_thread.start()
+    bot_thread.start()
+
+    # Ждем, пока оба потока завершат работу
+    server_thread.join()
+    bot_thread.join()
